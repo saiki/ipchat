de7cbf8f5ffbc019862b3e38f1c76bb833a400f0
Akio Saiki <akio.saiki@gmail.com>
2011-06-18 18:16:46 +0900

    ユーザー受付とコメント受付を別ポートに修正

--- js/index.js
+++ js/index.js
@@ -1,31 +1,43 @@
-var server = null;
+var loginServer = null;
+var recieveServer = null;
 var clients = new Object();
 
 $(function() {
-  server = new air.ServerSocket();
-  server.addEventListener("connect", onConnect);
-  server.bind(8081);
-  server.listen();
+  loginServer = new air.ServerSocket();
+  loginServer.addEventListener("connect", onLoginConnect);
+  loginServer.bind(6666);
+  loginServer.listen();
+  recieveServer = new air.ServerSocket();
+  recieveServer.addEventListener("connect", onRecieveConnect);
+  recieveServer.bind(6667);
+  recieveServer.listen();
   clients["127.0.0.1"] = true;
 
   $("#send").click(onComment);
 });
 
 $(window).unload(function() {
-  server.close();
+  loginServer.close();
+  recieveServer.close();
 });
 
-function onConnect(e) {
-  var socket = e.socket;
-  var address = socket.removeAddress;
-  if (address == undefined) {
-    address = socket.localAddress;
-  }
-  if (clients[address] == undefined) {
-    air.trace("undefined="+address);
-    clients[address] = true;
-  }
+function onLoginConnect(loginEvent) {
+  var socket = loginEvent.socket;
+  loginEvent.socket.addEventListener("socketData", function(e) {
+    var address = socket.removeAddress;
+    var userName = socket.readUTFBytes(socket.bytesAvailable);
+    if (address == undefined) {
+      address = socket.localAddress;
+    }
+    if (clients[address] == undefined) {
+      clients[address] = userName;
+    }
+    socket.close();
+  });
+}
 
+function onRecieveConnect(recieveEvent) {
+  var socket = recieveEvent.socket;
   socket.addEventListener("socketData", function(e) {
     var message = socket.readUTFBytes(socket.bytesAvailable);
     if (message != "") {
@@ -38,7 +50,7 @@ function onConnect(e) {
 function onComment() {
   for (address in clients) {
     var socket = new air.Socket();
-    socket.connect(address, 8081);
+    socket.connect(address, 6667);
     socket.writeUTFBytes($("input[name=comment]").val());
     socket.flush();
   }
